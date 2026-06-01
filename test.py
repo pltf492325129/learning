0e762a2a8319714fadd93600341c8d1bea2aea33
VoidTile <49930095+pltf492325129@users.noreply.github.com>
2026-06-01 16:36:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,57 @@
-#if CURRENT_LOG_LEVEL <= LOG_LEVEL_DEBUG
-    #define DBG_LOG(format, ...)                                            \
-        do {                                                                \
-            os::log("%s,%d: %s" format, SHORT_FILE, __LINE__, LOG_DEBUG_STAMP, ##__VA_ARGS__); \
-        } while (0)
-#else
-    #define DBG_LOG(format, ...)
-#endif
+	行 14896: 06-01 16:34:30.026 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18502]Depth overlay: FBO=77 has depth attachment type=0x1702
+	行 14910: 06-01 16:34:30.042 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18502]Depth overlay: FBO=33 has depth attachment type=0x1702
+	行 14911: 06-01 16:34:30.042 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,28981: [DEBUG] [DOV][F18502]Depth overlay: detected depth blit from FBO=77, mask=0x100 filter=2600
+	行 14916: 06-01 16:34:30.042 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18502]Depth overlay: FBO=77 has depth attachment type=0x1702
+	行 14919: 06-01 16:34:30.042 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18502]Depth overlay: FBO=78 has depth attachment type=0x1702
+	行 14976: 06-01 16:34:30.050 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18502]Depth overlay: FBO=96 has depth attachment type=0x1702
+	行 14988: 06-01 16:34:30.055 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18502]Depth overlay: FBO=45 has depth attachment type=0x1702
+	行 14992: 06-01 16:34:30.057 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18502]Depth overlay: FBO=6 has depth attachment type=0x1702
+	行 15022: 06-01 16:34:30.061 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18502]Depth overlay: FBO=1 has depth attachment type=0x8d41
+	行 15028: 06-01 16:34:30.066 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,41277: [DEBUG] [DOV][F18502]Depth overlay: selected best depth FBO=77 (draws=141, verts=479532)
+	行 15029: 06-01 16:34:30.066 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: depth_overlay.cpp,353: [DEBUG] [DOV][F18502] depth capTex=230 1918x828
+	行 15030: 06-01 16:34:30.067 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: depth_overlay.cpp,404: [DEBUG] [DOV][F18502] PlanA=0, try PlanB
+	行 15031: 06-01 16:34:30.068 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: depth_overlay.cpp,463: [DEBUG] [DOV][F18502] PlanB=0 (depth empty?)
+	行 15046: 06-01 16:34:30.078 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18503]Depth overlay: FBO=31 has depth attachment type=0x1702
+	行 15053: 06-01 16:34:30.092 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18503]Depth overlay: FBO=33 has depth attachment type=0x1702
+	行 15054: 06-01 16:34:30.093 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,28981: [DEBUG] [DOV][F18503]Depth overlay: detected depth blit from FBO=31, mask=0x100 filter=2600
+	行 15059: 06-01 16:34:30.093 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18503]Depth overlay: FBO=31 has depth attachment type=0x1702
+	行 15062: 06-01 16:34:30.093 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18503]Depth overlay: FBO=78 has depth attachment type=0x1702
+	行 15123: 06-01 16:34:30.102 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18503]Depth overlay: FBO=96 has depth attachment type=0x1702
+	行 15136: 06-01 16:34:30.108 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18503]Depth overlay: FBO=45 has depth attachment type=0x1702
+	行 15140: 06-01 16:34:30.110 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18503]Depth overlay: FBO=6 has depth attachment type=0x1702
+	行 15170: 06-01 16:34:30.114 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18503]Depth overlay: FBO=1 has depth attachment type=0x8d41
+	行 15177: 06-01 16:34:30.119 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,41277: [DEBUG] [DOV][F18503]Depth overlay: selected best depth FBO=31 (draws=141, verts=479532)
+	行 15178: 06-01 16:34:30.119 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: depth_overlay.cpp,353: [DEBUG] [DOV][F18503] depth capTex=230 1918x828
+	行 15179: 06-01 16:34:30.120 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: depth_overlay.cpp,404: [DEBUG] [DOV][F18503] PlanA=0, try PlanB
+	行 15180: 06-01 16:34:30.121 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: depth_overlay.cpp,463: [DEBUG] [DOV][F18503] PlanB=0 (depth empty?)
+	行 15198: 06-01 16:34:30.130 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18504]Depth overlay: FBO=77 has depth attachment type=0x1702
+	行 15210: 06-01 16:34:30.145 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18504]Depth overlay: FBO=33 has depth attachment type=0x1702
+	行 15211: 06-01 16:34:30.145 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,28981: [DEBUG] [DOV][F18504]Depth overlay: detected depth blit from FBO=77, mask=0x100 filter=2600
+	行 15216: 06-01 16:34:30.145 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18504]Depth overlay: FBO=77 has depth attachment type=0x1702
+	行 15219: 06-01 16:34:30.145 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18504]Depth overlay: FBO=78 has depth attachment type=0x1702
+	行 15276: 06-01 16:34:30.154 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18504]Depth overlay: FBO=96 has depth attachment type=0x1702
+	行 15288: 06-01 16:34:30.157 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18504]Depth overlay: FBO=45 has depth attachment type=0x1702
+	行 15292: 06-01 16:34:30.160 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18504]Depth overlay: FBO=6 has depth attachment type=0x1702
+	行 15322: 06-01 16:34:30.164 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18504]Depth overlay: FBO=1 has depth attachment type=0x8d41
+	行 15328: 06-01 16:34:30.169 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,41277: [DEBUG] [DOV][F18504]Depth overlay: selected best depth FBO=77 (draws=140, verts=476688)
+	行 15329: 06-01 16:34:30.169 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: depth_overlay.cpp,353: [DEBUG] [DOV][F18504] depth capTex=230 1918x828
+	行 15330: 06-01 16:34:30.170 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: depth_overlay.cpp,404: [DEBUG] [DOV][F18504] PlanA=0, try PlanB
+	行 15332: 06-01 16:34:30.171 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: depth_overlay.cpp,463: [DEBUG] [DOV][F18504] PlanB=0 (depth empty?)
+	行 15346: 06-01 16:34:30.180 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18505]Depth overlay: FBO=31 has depth attachment type=0x1702
+	行 15353: 06-01 16:34:30.194 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18505]Depth overlay: FBO=33 has depth attachment type=0x1702
+	行 15354: 06-01 16:34:30.194 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,28981: [DEBUG] [DOV][F18505]Depth overlay: detected depth blit from FBO=31, mask=0x100 filter=2600
+	行 15361: 06-01 16:34:30.194 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18505]Depth overlay: FBO=31 has depth attachment type=0x1702
+	行 15364: 06-01 16:34:30.194 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18505]Depth overlay: FBO=78 has depth attachment type=0x1702
+	行 15424: 06-01 16:34:30.203 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18505]Depth overlay: FBO=96 has depth attachment type=0x1702
+	行 15436: 06-01 16:34:30.208 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18505]Depth overlay: FBO=45 has depth attachment type=0x1702
+	行 15440: 06-01 16:34:30.210 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18505]Depth overlay: FBO=6 has depth attachment type=0x1702
+	行 15475: 06-01 16:34:30.214 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18505]Depth overlay: FBO=1 has depth attachment type=0x8d41
+	行 15481: 06-01 16:34:30.218 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,41277: [DEBUG] [DOV][F18505]Depth overlay: selected best depth FBO=31 (draws=143, verts=481074)
+	行 15482: 06-01 16:34:30.218 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: depth_overlay.cpp,353: [DEBUG] [DOV][F18505] depth capTex=230 1918x828
+	行 15483: 06-01 16:34:30.219 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: depth_overlay.cpp,404: [DEBUG] [DOV][F18505] PlanA=0, try PlanB
+	行 15484: 06-01 16:34:30.220 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: depth_overlay.cpp,463: [DEBUG] [DOV][F18505] PlanB=0 (depth empty?)
+	行 15499: 06-01 16:34:30.228 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18506]Depth overlay: FBO=77 has depth attachment type=0x1702
+	行 15513: 06-01 16:34:30.244 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18506]Depth overlay: FBO=33 has depth attachment type=0x1702
+	行 15514: 06-01 16:34:30.244 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,28981: [DEBUG] [DOV][F18506]Depth overlay: detected depth blit from FBO=77, mask=0x100 filter=2600
+	行 15519: 06-01 16:34:30.244 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18506]Depth overlay: FBO=77 has depth attachment type=0x1702
+	行 15522: 06-01 16:34:30.244 18299 21352 I A00368/com.miHoYo.Yuanshen/gltrace: tcp_egltrace_auto.cpp,1545: [DEBUG] [DOV][F18506]Depth overlay: FBO=78 has depth attachment type=0x1702
